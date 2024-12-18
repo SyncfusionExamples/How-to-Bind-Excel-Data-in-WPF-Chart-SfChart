7e3ed2003c06fcefcb673d45dacee614cf3be4fe
Priyadharshini-Sf4862 <priyadharshini.periyasamy@syncfusion.com>
2024-12-18 14:20:54 +0530

    WPF-Binding Excel data sample project added

---
+++ SfChart_BindExcelData/Model/ProductSales.cs
@@ -0,0 +1,8 @@
+﻿namespace SfChart_BindExcelData
+{
+    public class ProductSales
+    {
+        public string Month { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ SfChart_BindExcelData/ViewModel/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.XlsIO;
+using System.Collections.ObjectModel;
+
+namespace SfChart_BindExcelData
+{
+    public class ViewModel
+    {
+        public ObservableCollection<ProductSales> ProductAData { get; set; }
+        public ObservableCollection<ProductSales> ProductBData { get; set; }
+        public ObservableCollection<ProductSales> ProductCData { get; set; }
+
+        public ViewModel()
+        {
+            // Initialize data collections
+            ProductAData = new ObservableCollection<ProductSales>();
+            ProductBData = new ObservableCollection<ProductSales>();
+            ProductCData = new ObservableCollection<ProductSales>();
+
+            // Load Excel data
+            LoadExcelData("Data.xlsx");
+        }
+
+        private void LoadExcelData(string filePath)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                IWorkbook workbook = application.Workbooks.Open(filePath);
+                IWorksheet worksheet = workbook.Worksheets[0]; // First worksheet
+                int lastRow = worksheet.UsedRange.LastRow; // Get the last row with data dynamically
+                for (int i = 2; i <= lastRow; i++) // Assuming headers are in Row 1
+                {
+                    string month = worksheet[$"A{i}"].Text;
+                    double value1 = worksheet[$"B{i}"].Number;
+                    double value2 = worksheet[$"C{i}"].Number;
+                    double value3 = worksheet[$"D{i}"].Number;
+
+                    ProductAData.Add(new ProductSales { Month = month, Value = value1 });
+                    ProductBData.Add(new ProductSales { Month = month, Value = value2 });
+                    ProductCData.Add(new ProductSales { Month = month, Value = value3 });
+                }
+
+                workbook.Close();
+            }
+        }
+    }
+}
